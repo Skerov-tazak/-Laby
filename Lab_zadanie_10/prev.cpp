d5e5b5945b7b7a7c4a92d354a72bc6bdf33f5064
Skerov-tazak <sqerow@gmail.com>
2024-12-29 18:25:07 +0100

    this is laptop 29th december commit

--- Lab_zadanie_10/prev.cpp
+++ Lab_zadanie_10/prev.cpp
@@ -1,7 +1,5 @@
 #include "prev.h"
 #include<climits>
-#include <cstddef>
-#include <iterator>
 #include<memory>
 #include<iostream>
 
@@ -16,20 +14,20 @@ struct treepathNode
 {
 	intrv interval;
 	int best_index;
-	std::shared_ptr<treepathNode> next_node;
-	std::shared_ptr<treepathNode> sibling;
+	std::shared_ptr<treepathNode> right;
+	std::shared_ptr<treepathNode> left;
 
-	treepathNode(intrv _interval, int _best_index, std::shared_ptr<treepathNode> _next_node, std::shared_ptr<treepathNode> _sibling)
+	treepathNode(intrv _interval, int _best_index, std::shared_ptr<treepathNode> _right, std::shared_ptr<treepathNode> _left)
 	{
 		interval = _interval;
 		best_index = _best_index;
-		sibling = _sibling;
-		next_node = _next_node;
+		left = _left;
+		right = _right;
 	}
 
 	void dump()
 	{
-		std::cout << "{" << " {" << interval.low << "," << interval.high << "}, " << best_index << ", " << sibling << ", " << next_node << " }" << std::endl;
+		std::cout << "{" << " {" << interval.low << "," << interval.high << "}, " << best_index << ", " << left << ", " << right << " }" << std::endl;
 	}
 };
 
@@ -38,8 +36,6 @@ typedef struct
 	int value;
 	std::shared_ptr<treepathNode> path_start;
 }elem;
-
-
 
 std::vector<elem> trees;
 
@@ -55,17 +51,16 @@ void moveMirror(std::shared_ptr<treepathNode> &mirror_node, std::shared_ptr<tree
 {
 		if(mirror_node != nullptr)
 		{
-			if(isEqual(mirror_node->interval, current->interval))
+			if(isEqual(mirror_node->right->interval, current->right->interval))
 			{
-				current->sibling = mirror_node->sibling;
+				current->left = mirror_node->left;
+				mirror_node = mirror_node->right;
 			}
 			else 
 			{
-				current->sibling = mirror_node;
-				mirror_node = mirror_node->sibling;
+				current->left = mirror_node->right;
+				mirror_node = mirror_node->left;
 			}
-			if(mirror_node != nullptr)
-				mirror_node = mirror_node->next_node;
 		}
 }
 
@@ -90,7 +85,7 @@ std::shared_ptr<treepathNode> genPath(int i, int x)
 	current = begining;
 	current_intrv = newInterval(-1, x, current_intrv); // the first intervals (-N, -1) and (0, N) need to be created seperately due to overflows
 	current = std::make_shared<treepathNode>(current_intrv, i, nullptr, nullptr);
-	begining->next_node = current;
+	begining->right = current;
 	moveMirror(mirror_node, begining); // moves the mirror pointer at the begining
 	
 	while(current_intrv.high != current_intrv.low) // goes down the tree, finding the proper element
@@ -98,10 +93,8 @@ std::shared_ptr<treepathNode> genPath(int i, int x)
 		int mid = current_intrv.low - (current_intrv.low - current_intrv.high) / 2; // avoids overflows
 		current_intrv = newInterval(mid, x, current_intrv);
 		std::shared_ptr<treepathNode> next_node = std::make_shared<treepathNode>(current_intrv, i, nullptr, nullptr);
-		current->next_node = next_node;
-
+		current->right = next_node;
 		moveMirror(mirror_node, current); // moves the mirror pointer after every iteration and connects the current tree to the forest
-		current->dump();
 		current = next_node;
 	}
 
@@ -109,75 +102,77 @@ std::shared_ptr<treepathNode> genPath(int i, int x)
 	return begining;
 }
 
-
 void init(const std::vector<int> &seq)
 {
 	for(int i = 0; i < seq.size(); i++)
 		pushBack(seq[i]);
 }
 
-bool contains(intrv interval, int x)
+bool contains(intrv bigger, std::shared_ptr<treepathNode> contained)
 {
-	if(x >= interval.low && x <= interval.high)
+	if(contained == nullptr)
+		return false;
+
+	if(contained->interval.low >= bigger.low && contained->interval.high >= bigger.high)
 		return true;
 	else 
 		return false;
 }
 
-bool inRange(bool isLow, int value, intrv interval)
+bool overlaps(intrv a, std::shared_ptr<treepathNode> current)
 {
-	if(isLow)
-	{
-		if(interval.high >= value)
-			return true;
-	}
-	else 
-	{
-		if(interval.low <= value)
-			return true;
-	}
-	return false;
+	if(current == nullptr)
+		return false;
+
+	int	interval_end = std::max(a.low, current->interval.low); 
+	int interval_start = std::min(a.high, current->interval.high);
+
+	if(interval_start <= interval_end)
+		return true;
+	else
+		return false;
 }
 
-std::shared_ptr<treepathNode> iterate(bool isLow, int value, std::shared_ptr<treepathNode> current)
+int prevInRange_helper(std::shared_ptr<treepathNode> current, intrv range)
 {
-	std::cout<<"#############################" << std::endl;
-	if(isLow)
-	{
-		std::cout<< "finding for low at: " << value << std::endl;
-	}
-	else 
-	{
-		std::cout<< "finding for high at: " << value << std::endl;
+	if(current == nullptr)
+		return -1;
 
-	}
-	while(current->sibling != nullptr || current->next_node != nullptr)
+	int current_best = -1;
+	std::shared_ptr<treepathNode> current2 = nullptr;
+
+	while(current->interval.low != current->interval.high)
 	{
-		current->dump();
-		if(contains(current->next_node->interval, value))
-			current = current->next_node;
-		else if(current->sibling != nullptr && contains(current->sibling->interval, value))
-			current = current->sibling;
-		else if(inRange(isLow, value, current->next_node->interval))
-			current = current->next_node;
-		else if(current->sibling != nullptr && inRange(isLow, value, current->sibling->interval))
-			current = current->sibling;
-		else 
-			return current;
+		if(overlaps(range, current->right))
+		{
+			if(contains(range, current->left))
+			{
+				if(current_best <= current->left->best_index)
+					current_best = current->left->best_index;
+			}
+			else if(overlaps(range, current->left))
+			{
+				current2 = current->left;
+			}
+			current = current->right;
+		}
+		else if(overlaps(range, current->left))
+		{
+			current = current->left;
+		}
 	}
-	current->dump();
-	return current;
+
+	if(current->best_index > current_best)
+		current_best = current->best_index;	
+		
+	return std::max(current_best,prevInRange_helper(current2, range));
 }
-
 
 int prevInRange(int i, int lo, int hi)
 {
-	std::shared_ptr<treepathNode> current = trees[i].path_start;
-	std::shared_ptr<treepathNode> low = iterate(true, lo, current);
-	std::shared_ptr<treepathNode> high = iterate(false, hi, current);
-	
-
-
+	std::shared_ptr<treepathNode> current = trees[i].path_start;	
+	intrv range = {lo, hi};
+	return prevInRange_helper(current, range);
 }
 
 void pushBack(int value)
